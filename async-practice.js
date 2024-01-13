47db93303c2151fad1939ba805cbb811a03c2c37
jaydgm <rjayd22@gmail.com>
2024-01-12 16:18:16 -0800

    Initiate files to repo

---
+++ async-practice.js
@@ -0,0 +1,114 @@
+// exp 1 
+
+// exp of sync program
+// caller still needs to wait for function to
+// finish before caller can continue
+function makeGreeting(name) {
+    return 'Hello, my name is ${name}!';
+}
+
+const name = "Miriam";
+const greeting = 'Hello, my name is ${name}!';
+console.log(greeting);
+
+// exp 2
+// long running sync function
+// the higher the prime number the longer it takes to generate
+// another exp of it being sync function is when you try to do
+// anything while function is running, it will be unresponsive
+const MAX_PRIME = 1000000;
+
+function isPrime(n) {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return n > 1;
+}
+
+const random = (max) => Math.floor(Math.random() * max);
+
+function generatePrimes(quota) {
+  const primes = [];
+  while (primes.length < quota) {
+    const candidate = random(MAX_PRIME);
+    if (isPrime(candidate)) {
+      primes.push(candidate);
+    }
+  }
+  return primes;
+}
+
+const quota = document.querySelector("#quota");
+const output = document.querySelector("#output");
+
+document.querySelector("#generate").addEventListener("click", () => {
+  const primes = generatePrimes(quota.value);
+  output.textContent = `Finished generating ${quota.value} primes!`;
+});
+
+document.querySelector("#reload").addEventListener("click", () => {
+  document.location.reload();
+});
+
+// exp 3
+// event handlers are a form of async programming
+const log = document.querySelector(".event-log");
+
+document.querySelector("#xhr").addEventListener("click", () => {
+  log.textContent = "";
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("loadend", () => {
+    log.textContent = `${log.textContent}Finished with status: ${xhr.status}`;
+  });
+
+  xhr.open(
+    "GET",
+    "https://raw.githubusercontent.com/mdn/content/main/files/en-us/_wikihistory.json",
+  );
+  xhr.send();
+  log.textContent = `${log.textContent}Started XHR request\n`;
+});
+
+document.querySelector("#reload").addEventListener("click", () => {
+  log.textContent = "";
+  document.location.reload();
+});
+
+// exp 4
+// event handlers are a type of callback
+// callbacks are functions passed into 
+// another function
+// this exp can show how deeply nested callbacks
+// can be complex and harder to debug
+function doStep1(init, callback) {
+    const result = init + 1;
+    callback(result);
+  }
+  
+  function doStep2(init, callback) {
+    const result = init + 2;
+    callback(result);
+  }
+  
+  function doStep3(init, callback) {
+    const result = init + 3;
+    callback(result);
+  }
+  
+  function doOperation() {
+    doStep1(0, (result1) => {
+      doStep2(result1, (result2) => {
+        doStep3(result2, (result3) => {
+          console.log(`result: ${result3}`);
+        });
+      });
+    });
+  }
+  
+  doOperation();
+  
+
